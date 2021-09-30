fba303ede94dd7abe2148f331d95553abddb80ad
Hernan Gomez Molino <hernangomezmolino@gmail.com>
2021-09-29 22:16:20 -0300

    Agrego lo trabajado en clase el 29/09/2021.

--- app/inc/c_func.h
+++ app/inc/c_func.h
@@ -6,3 +6,7 @@
 uint32_t c_sum (uint32_t firstOperand, uint32_t secondOperand);
 
 void c_zeros(uint32_t *, uint32_t);
+
+void escalar32(uint32_t *,uint32_t *, uint32_t , uint32_t );
+
+void escalar16(uint16_t *,uint16_t *, uint32_t , uint32_t );
--- app/src/c_func.c
+++ app/src/c_func.c
@@ -12,3 +12,21 @@ void c_zeros(uint32_t *vector, uint32_t longitud)
 	    for(i = 0; i < longitud; i++)
 	    	vector[i] = 0;
 }
+
+void escalar32(uint32_t *vectorIn,uint32_t *vectorOut, uint32_t longitud, uint32_t factor)
+{
+	uint32_t i = 0 ;
+	for(i=0;i<longitud;i++);
+	{
+		vectorOut[i] = vectorIn[i]*factor;
+	} 
+}
+
+void escalar16(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud , uint32_t factor)
+{
+	uint32_t i = 0 ;
+	for(i=0;i<longitud;i++);
+	{
+		vectorOut[i] = vectorIn[i]*factor;
+	} 
+}
